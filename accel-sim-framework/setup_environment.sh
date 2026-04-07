88f9fdaa5de233a399067115325258c7a8de1a47
jorgenfinsveen <jorgen.finsveen@ntnu.no>
2026-04-07 09:59:56 +0200

    improvements

--- accel-sim-framework/setup_environment.sh
+++ accel-sim-framework/setup_environment.sh
@@ -5,6 +5,7 @@ export CUSTOM_SETUP_ENVIRONMENT_WAS_RUN=
 # Build and session management
 export ROOT="$HOME/projects/crisp_framework"
 export MESA_SIM="$ROOT/mesa-vulkan-sim"
+export VULKAN_SIM="$ROOT/vulkan-sim"
 export ACCEL_SIM="$ROOT/accel-sim-framework"
 export ACCELSIM_ROOT="$ACCEL_SIM"
 export VK_ICD_FILENAMES="$MESA_SIM/lib/share/vulkan/icd.d/lvp_icd.x86_64.json"
@@ -102,7 +103,7 @@ run() {
 	export -f source_all_environments
 
 	rm -rf "$ACCEL_SIM/logs" && mkdir -p "$ACCEL_SIM/logs"
-	
+
 	nohup bash -c 'set_sim' > "$ACCEL_SIM/logs/out.log" 2> "$ACCEL_SIM/logs/err.log" &
 	echo "Simulator started with PID $!"
 }
